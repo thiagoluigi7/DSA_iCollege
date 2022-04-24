c9c549e54fe8027113d8b879927c257f4d53f3ed
souamandaleal <a.amandaleal@hotmail.com>
2022-04-24 20:40:41 -0300

    Atualizando páginas home e área logada

--- frontend/src/views/LogadoView.vue
+++ frontend/src/views/LogadoView.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="arealogada">
     <NavbarLogado />
+    <HeroLogado />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import NavbarLogado from "@/components/NavbarLogado.vue";
+import HeroLogado from "@/components/HeroLogado.vue";
 
 export default {
   name: "HomeView",
   components: {
     NavbarLogado,
+    HeroLogado,
   },
 };
 </script>
