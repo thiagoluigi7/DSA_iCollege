946f463762433ef37ef05cd12368e2d456255a32
souamandaleal <a.amandaleal@hotmail.com>
2022-04-24 19:07:35 -0300

    Adicionando página logado

---
+++ frontend/src/views/LogadoView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="arealogada">
+    <NavbarLogado />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavbarLogado from "@/components/NavbarLogado.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    NavbarLogado,
+  },
+};
+</script>
